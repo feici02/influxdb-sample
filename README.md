09ea7a17b50a382d2b8ba5e62c3271f1af65e794
WANG Xu <feici02@outlook.com>
2023-05-24 16:04:33 +0800

    feat: add -p option

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 ```commandline
 python main.py -h
-usage: main.py [-h] [-w] [-c COUNT]
+usage: main.py [-h] [-w] -c COUNT -p {s,ms,us,ns}
 
 optional arguments:
   -h, --help            show this help message and exit
   -w, --write           write
   -c COUNT, --count COUNT
                         count, 0 for non-stop
+  -p {s,ms,us,ns}, --precision {s,ms,us,ns}
+                        time precision
 ```
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import argparse
 import configparser
-import datetime
+import time
 
 import influxdb_client
-import time
-from influxdb_client import Point
+from influxdb_client import Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
@@ -21,22 +20,33 @@ def parse_config():
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--write", help="write", action="store_true")
-    parser.add_argument("-c", "--count", help="count, 0 for non-stop", type=int)
+    parser.add_argument("-c", "--count", help="count, 0 for non-stop", type=int, required=True)
+    parser.add_argument("-p", "--precision", help="time precision", choices=["s", "ms", "us", "ns"], required=True)
     return parser.parse_args()
 
 
-def write_influxdb(write_api, org, bucket, i):
+def write_influxdb(write_api, org, bucket, i, precision):
     point = (
-        Point("testdata")
+        Point("xwang2")
         .tag("tagname1", "tagvalue1")
-        .tag("tagname2", "tagvalue2")
-        .tag("tagname3", "tagvalue3")
+        .tag("tagname2", 2.0)
+        .tag("tagname3", 3)
+        .tag("tagname4", True)
         .field("field1", i)
         .field("field2", "hello" + str(i))
         .field("field3", True if i % 2 == 0 else False)
-        .time(datetime.datetime.now())
+        .field("field4", float(i))
     )
-    print(point)
+
+    if precision == "s":
+        point.time(int(time.time_ns() / 1000000000), write_precision=WritePrecision.S)
+    elif precision == "ms":
+        point.time(int(time.time_ns() / 1000000), write_precision=WritePrecision.MS)
+    elif precision == "us":
+        pass
+        point.time(int(time.time_ns() / 1000), write_precision=WritePrecision.US)
+    elif precision == "ns":
+        point.time(int(time.time_ns()), write_precision=WritePrecision.NS)
 
     write_api.write(org=org, bucket=bucket, record=point)
 
@@ -55,7 +65,7 @@ def main():
                 break
 
             print(f"write {i}")
-            write_influxdb(write_api, org, bucket, i)
+            write_influxdb(write_api, org, bucket, i, args.precision)
             i += 1
             time.sleep(1)  # separate points by 1 second
 
